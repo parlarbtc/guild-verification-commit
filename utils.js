2a6f36b31c187b4aa98db417fb19232a519547fe
parlar <129975026+parlarbtc@users.noreply.github.com>
2025-11-02 13:04:41 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-02T10:04:06.362Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-02T10:04:40.509Z
+Commit: Add export functionality for reports
