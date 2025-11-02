9b95e92e99b88afeb7979451f1376848b5103f2d
parlar <129975026+parlarbtc@users.noreply.github.com>
2025-11-02 13:05:57 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-02T10:05:52.353Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-02T10:05:56.280Z
+Commit: Patch API endpoint error handling
